1e04d6072967b503a8ae019c68a6166b21af5d30
DESKTOP-PB8OQ0H\właściciel <filip_janek@interia.pl>
2017-07-11 19:21:24 +0200

    Position report works, door is placed.

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
-
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -18,8 +18,10 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
+	FString OwnerName = GetOwner()->GetName();
+	FVector Position = GetOwner()->GetActorTransform().GetLocation();
+	FString OwnerPos = GetOwner()->GetTransform().GetLocation().ToString();//"X = " + Position.X + ", Y = " + Position.Y + ", Z = " + Position.Z;
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s!"),*OwnerName, *OwnerPos);
 	
 }
 
